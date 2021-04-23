010204e2bd3d67fd3d218bb6aa4f3b5ecd82289a
Seba <Seba@Seba-PC.lan>
2021-04-23 14:20:47 -0300

    Version 0.0.1

---
+++ src/preparcial/Imagenes.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package preparcial;
+
+/**
+ *
+ * @author Seba y Mauro :D
+ */
+public class Imagenes {
+    // atributos
+    private String src;
+    
+    // Constructor
+    public Imagenes (String src){
+        
+    }
+    
+    // Metodos
+    public String getSrc(){
+    return src;
+    }
+    
+    public void setSrc(String src){
+    this.src = src;
+    }
+}
---
+++ src/preparcial/PreParcial.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package preparcial;
+import java.util.Scanner;
+/**
+ *
+ * @author Seba y Mauro :D
+ */
+public class PreParcial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}
